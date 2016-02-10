c71c063de4af38e5f5473c74a55a3915917d475a
Jekaterina Vorošilova <jekavor@greeny.cs.tlu.ee>
2016-02-10 11:31:30 +0200

    new files

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-hj
+hj(function (){
+  "use strict";
+
+   var Moosipurk = function(){
+
+     //SINGLETON PATTERN
+   if(Moosipurk.instance){
+
+       return Moosipurk.instance;
+     }
+
+Moosipurk.instance = this; // this viitab Moosipurgile
+
+       console.log(this);
+  //   console.log('Moosipurgi sees');
+
+ //Koik muutujad mis on uldiselt
+ this.click_count = 0;
+
+  //panen rakenduse toole
+    this.event;
+   };
+
+//koik moosipurgi finktsioonid tulevad siia sisse
+Moosipurk.prototype = {
+        init: function (){
+          console.log("Rakendus kaivitus");
+          //hakkkame kuulama hiireklopse
+        //  this.bindMouseEvents();
+
+        },
+        bindMouseEvent: function (){
+          //querySelector - saadab kätte nuppu
+          document.querySelector(".add-new-jar").addEventListener("click", this.addNewClick.bind(this));
+        },
+        addNewClick: function (event){
+          console.log(event);
+
+          this.click_count++;
+          console.log(this.click_count);
+        }
+};
+
+
+ window.onload = function(){
+    var app = new Moosipurk;
+
+ };
+
+})();
